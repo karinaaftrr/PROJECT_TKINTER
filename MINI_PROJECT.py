7939cd210e3b16bc89255990530b53314490c463
ikbaleeee <ikbalferia@gmail.com>
2025-05-30 20:11:28 +0700

    MINI_PROJECT.py

--- MINI_PROJECT.py
+++ MINI_PROJECT.py
@@ -96,3 +96,43 @@ class SecondPage:
         self.button_window = None
         self.update_background()
         self.root.bind("<Configure>", self.on_resize)
+        
+    def update_background(self):
+        w = self.root.winfo_width()
+        h = self.root.winfo_height()
+
+        if w > 1 and h > 1:
+            resized = self.original_image.resize((w, h), Image.LANCZOS)
+            self.bg_photo = ImageTk.PhotoImage(resized)
+            self.canvas.delete("all")
+            self.canvas.create_image(0, 0, image=self.bg_photo, anchor="nw")
+
+            logo_image = Image.open("Logo_Unila.png").resize((200, 150), Image.LANCZOS)
+            self.logo_photo = ImageTk.PhotoImage(logo_image)
+            self.canvas.create_image(10, 10, image=self.logo_photo, anchor="nw")
+
+            rama_image = Image.open("rama praditha.jpg").resize((100,150), Image.LANCZOS)
+            self.rama_photo = ImageTk.PhotoImage(rama_image) 
+            self.canvas.create_image(350, 200, image=self.rama_photo, anchor="nw")
+
+            reggy_image = Image.open("reggy2.jpg").resize((100,150), Image.LANCZOS)
+            self.reggy_photo = ImageTk.PhotoImage(reggy_image) 
+            self.canvas.create_image(500, 200, image=self.reggy_photo, anchor="nw")
+
+            karina_image = Image.open("karina3.jpg").resize((100,150), Image.LANCZOS)
+            self.karina_photo = ImageTk.PhotoImage(karina_image) 
+            self.canvas.create_image(650, 200, image=self.karina_photo, anchor="nw")
+
+            ikbal_image = Image.open("ikbal.jpg").resize((100,150), Image.LANCZOS)
+            self.ikbal_photo = ImageTk.PhotoImage(ikbal_image) 
+            self.canvas.create_image(800, 200, image=self.ikbal_photo, anchor="nw")
+
+            text = " ".join("Our Team!")
+            font_style = ("Impact", 44, "bold")
+            self.canvas.create_text(w * 0.5, h * 0.15, text=text, font=font_style, fill="white", anchor="center")
+
+            text = "KELOMPOK 6"
+            font_style = ("Comic Sans MS", 22, "bold")
+            self.canvas.create_text(w * 0.5, h * 0.25, text=text, font=font_style, fill="white", anchor="center")
+
+            self.button_window = self.canvas.create_window(w - 20, 20, window=self.button_frame, anchor="ne")
