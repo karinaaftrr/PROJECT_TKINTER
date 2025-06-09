17490ed0d437ca31f340d693654ae9f32ceefe6a
karinaaftrr <karinafitriamalia05@gmail.com>
2025-06-09 23:32:12 +0700

    Update MINI_PROJECT.py

--- MINI_PROJECT.py
+++ MINI_PROJECT.py
@@ -154,6 +154,49 @@ class SecondPage:
 
     def exit_app(self):
         self.root.destroy()
+        
+class ThirdPage:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Third Page")
+        self.root.state("zoomed")
+
+        self.canvas = tk.Canvas(self.root, highlightthickness=0)
+        self.canvas.pack(fill="both", expand=True)
+
+        self.original_image = Image.open("Bendera.png")
+        self.bg_photo = None
+        self.logo_photo = None
+
+        self.frame_kanan = tk.Frame(self.root, bg="#111")
+        self.frame_kanan.place(relx=0.6, rely=0.35, anchor="w")
+
+        self.score_kanan = 0
+        self.setup_score_section_kanan(self.frame_kanan)
+
+        self.frame_kiri = tk.Frame(self.root, bg="#111")
+        self.frame_kiri.place(relx=0.15, rely=0.35, anchor="e")
+
+        self.score_kiri = 0
+        self.setup_score_section_kiri(self.frame_kiri)
+
+        self.button_frame = tk.Frame(self.root, bg="#06075C", bd=0)
+
+        self.back_button = tk.Button(
+            self.button_frame, text="BACK", font=("Segoe UI", 12, "bold"),
+            bg="#767b21", fg="white", width=10, command=self.before_page
+        )
+        self.exit_button = tk.Button(
+            self.button_frame, text="EXIT", font=("Segoe UI", 12, "bold"),
+            bg="#dc3545", fg="white", width=10, command=self.exit_app
+        )
+        self.back_button.pack(side="left", padx=5)
+        self.exit_button.pack(side="left", padx=5)
+
+        self.button_window = None
+        self.update_background()
+        self.root.bind("<Configure>", self.on_resize)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
